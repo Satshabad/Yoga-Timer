e13120836ea5854ad819fa0ba9bb2ba85fc9c55c
satshabad <satshabad.music@gmail.com>
2012-02-18 00:07:11 -0800

    Added class for user to choose saved sets from

---
+++ Yoga Timer/src/net/satshabad/android/yogatimer/SavedSetChooserActivity.java
@@ -0,0 +1,14 @@
+package net.satshabad.android.yogatimer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SavedSetChooserActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+}
